5af97560845b085c7f9b7d90e8b7f1140b69d7ef
Shweta Panditrao <panditraos@yahoo-inc.com>
2015-10-31 09:29:52 -0700

    update

--- notes.js
+++ notes.js
@@ -94,10 +94,12 @@
       }
 
       var searchTerm = notesSearchFilter.val().toLowerCase();
-      $('body').append(JSON.stringify(notes));
+
       var filteredNotes = notes.filter(function (note) {
         return searchTerm ? note.noteText.toLowerCase().indexOf(searchTerm) > -1 : true;
       });
+
+      $('body').append(JSON.stringify(filteredNotes) ' .');
 
       var filteredNotes = filteredNotes.sort(function (note1, note2) {
         return asc ? note1[orderBy] > note2[orderBy] : note1[orderBy] < note2[orderBy];
